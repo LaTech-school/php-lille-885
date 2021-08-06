664365989a384750f73162266ee898efe05bd606
Arnaud BODEL <contact@osw3.net>
2021-08-06 10:32:06 +0200

    -

---
+++ bdd/config.php
@@ -0,0 +1,27 @@
+<?php 
+
+// Database connexion ID's
+// --
+
+// Type de base de données
+$db_type = "mysql";
+
+// Adresse du serveur de base de données
+$db_host = "127.0.0.1";
+
+// Port de connexion à la base de données
+$db_port = 3306;
+
+// Nom d'utilisateur de la base de données
+// Nom d'utilisateur par defaut sous XAMPP : root
+$db_user = "osw3"; 
+
+// Mot de passe associé à l'utilisateur de la base de données
+// Mot de passe par defaut sous XAMPP : "" une chaine vide
+$db_password = "myosw3sql";
+
+// Schema de base de données
+$db_schema = "cours_php_bdd";
+
+// Jeu de caractère de la base de données
+$db_charset = "utf8";
